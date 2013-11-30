d1c694160a830e1272376a92968ba31d776c1cad
chunyang.wei <chunyang.wei@hulu.com>
2013-11-29 20:51:21 -0800

    Bug fix in save_model

--- DT/model.py
+++ DT/model.py
@@ -15,14 +15,16 @@ class DT_Model:
         return
     
     def save_model(self, data_path):
-        f_model = open(data_path)
-        save_tree(self.root, 0, f_model)
+        f_model = open(data_path, "w")
+        self.save_tree(self.root, 0, f_model)
 
     def save_tree(self, root, height, f_model):
         if root.is_leaf:
-            print >>f_model, "".join(["\t"] * height), self.data_count, self.prob 
+            print >>f_model, "".join([" "] * height), root.data_count, root.prob 
             return
-        print >>f_model, "".join(["\t"] * height), self.data_count, self,split_feature, self.split_feature_threshold 
+        print >>f_model, "".join([" "] * height), root.data_count, root.split_feature.id, \
+                root.split_feature_threshold 
+        print >>f_model, "(", root.split_value_2_index, ")"
         
-        for sub in self.children:
+        for sub in root.children:
             self.save_tree(sub, height + 1, f_model)
