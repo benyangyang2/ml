65f3920133eaa5a08567c25a7c8925d15e65fd87
chunyang.wei <chunyang.wei@hulu.com>
2013-11-29 20:51:58 -0800

    sample parser

--- DT/sample.py
+++ DT/sample.py
@@ -4,6 +4,7 @@ class DT_Sample:
         self.features = {}
         self.parse(line)
 
+    """
     def parse(self, line):
         fields = line.split()
         self.label = fields[14] == "1"
@@ -11,6 +12,39 @@ class DT_Sample:
         for f in fields[1:13]:
            self.features[f_id] = f
            f_id += 1
+    """
+    """ 
+    def parse(self, line):
+        fields = line.strip().split(",")
+        self.label = fields[16] == "democrat."
+        f_id = 0
+        for f in fields[0:16]:
+            self.features[f_id] = f
+            f_id += 1
+    """
+    def parse(self, line):
+        fields = line.split()
+        self.label = fields[0] == "1"
+        for f in fields[2:]:
+            f_id = int(f.split(":")[0])
+            f_value = f.split(":")[1]
+            self.features[f_id] = f_value
+    
+class BinarySample:
+    def __init__(self, line, feature_list):
+        self.label = None
+        self.features = {}
+        self.parse(line, feature_list)
+
+    def parse(self, line, feature_list):
+        fields = line.split()
+        self.label = fields[0] == "1"
+        for f in feature_list:
+            self.features[f] = "0"
+        for f in fields[2:]:
+            f_id = int(f.split(":")[0])
+            #f_value = f.split(":")[1]
+            self.features[f_id] = "1"
             
 
 def get_pos_count(dataset):
